2ab40d9fa3a4617d92e6a3bebd8f12642f988bb0
akmal2409 <akmalalikhujaev@gmail.com>
2020-09-12 17:22:47 +0200

    Added ingredients to a recipe and displayed them

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from "@angular/core";
 import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
 
 @Injectable({providedIn: 'root'})
 export class RecipeService {
@@ -7,8 +8,18 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe("A test recipe", "This is simply a test",
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe("My Meat", "This is simply a test",
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient("Potatoes", 2)
+      ]),
+    new Recipe("Chorizo & mozzarella gnocchi bake", "This is simply a test",
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=408%2C370',
+      [
+        new Ingredient('Mozarella', 1),
+        new Ingredient("Potatoes", 2)
+      ])
   ];
 
   getRecipes() {
